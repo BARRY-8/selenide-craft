1a02fc60b30762ad5d1a95720352250d2c98f6be
BHARATH <86478568+B-H-A-R-A-T-H@users.noreply.github.com>
2023-12-29 10:06:26 +0530

    Update Main.java

--- src/main/java/com/automation/main/Main.java
+++ src/main/java/com/automation/main/Main.java
@@ -3,5 +3,6 @@ package com.automation.main;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("selenide");
     }
 }
